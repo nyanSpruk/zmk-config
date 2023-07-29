eac55d418841c3495d9c44c7a496a19b5317a905
NyanSpruk <slimenian@gmail.com>
2023-07-29 20:48:53 +0200

    Test'

--- config/keys_si.h
+++ config/keys_si.h
@@ -5,10 +5,10 @@
  */
 #pragma once
 
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
-#include <dt-bindings/zmk/modifiers.h>
-#include <dt-bindings/zmk/keys.h>
+// #include <dt-bindings/zmk/hid_usage.h>
+// #include <dt-bindings/zmk/hid_usage_pages.h>
+// #include <dt-bindings/zmk/modifiers.h>
+// #include <dt-bindings/zmk/keys.h>
 
 /*
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
